a491e7f9dd39ce4d39cb8e823ddc4da065340e50
AlexandraLukacs <lszandika15@gmail.com>
2024-12-09 09:27:09 +0100

    f3

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -3,4 +3,10 @@ package hu.szamalk;
 public class Tabla {
     private char[][] t;
     private char uresCella;
+
+    public Tabla(char uresCella) {
+        t[7][7] = uresCella;
+        this.uresCella = uresCella;
+    }
 }
+
