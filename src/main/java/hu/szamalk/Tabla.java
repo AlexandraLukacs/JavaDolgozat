36de2764e70cd41ac3fc5fe7304cf28b5549a629
AlexandraLukacs <lszandika15@gmail.com>
2024-12-09 09:38:34 +0100

    f4

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -8,5 +8,16 @@ public class Tabla {
         t[7][7] = uresCella;
         this.uresCella = uresCella;
     }
+
+    public void megjelenit(){
+        Tabla tabla = new Tabla('#');
+        for (int sor = 0; sor < t.length; sor++) {
+            for (int oszl = 0; oszl < t.length; oszl++) {
+                System.out.println(t[sor][oszl]);
+            }
+        }
+    }
+
+    
 }
 
